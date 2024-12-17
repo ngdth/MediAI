7a9a3ceb5c72c611460d3c7c6d483704a8dec19f
ngdth <thienndde170807@fpt.edu.vn>
2024-12-17 23:34:14 +0700

    test

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import { AuthProvider } from 'contexts/AuthContext'
+import { BrowserRouter } from 'react-router-dom'
 import App from 'App'
 import 'styles/index.css'
 
@@ -11,8 +12,10 @@ const root = ReactDOM.createRoot(element)
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <CssBaseline />
-      <App />
+      <BrowserRouter>
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 )
