c199c08b7ef01a5979254493a883858260fa8585
Anh <ngocanhtk04@gmail.com>
2025-02-14 23:54:07 +0700

    delete home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,36 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Layout from './Components/Layout/Layout';
-import LayoutWithoutHeader from './components/Layout/LayoutWithoutHeader';
-import MainHome from './Pages/HomePage/MainHome';
-import HomeV2 from './Pages/HomePage/HomeV2';
-import HomeV3 from './Pages/HomePage/HomeV3';
-import AboutPage from './Pages/AboutPage/AboutPage';
-import LoginPage from './pages/AuthenPage/LoginPage';
-import SignUpPage from './pages/AuthenPage/SignUpPage';
-import ServicePage from './Pages/Service/ServicePage';
-import ServiceDetails from './Pages/Service/ServiceDetails';
-import BlogsPage from './Pages/BlogsPage/BlogsPage';
-import BlogsDetails from './Pages/BlogsPage/BlogsDetails';
-import DoctorsPage from './Pages/Pages/DoctorsPage';
-import DoctorsDetailsPage from './Pages/Pages/DoctorsDetailsPage';
-import TimeTablePage from './Pages/Pages/TimeTablePage';
-import PortfolioPage from './Pages/Pages/PortfolioPage';
-import ErrorPage from './Pages/Pages/ErrorPage';
-import ContactPage from './Pages/ContactPage/ContactPage';
-import Appointments from './Pages/Pages/Appointments';
-import AppointmentsHistory from './Pages/Pages/AppointmentsHistory';
-import UpdateAppointment from './Pages/Pages/UpdateAppointment';
-import DoctorAppointments from './Pages/Pages/DoctorAppointments';
-import AppointmentDetail from './Pages/Pages/AppointmentDetail';
-import ScrollUpButton from './Components/ScrollUpButton';
-import DoctorsResultPage from './Pages/Pages/DoctorsResultPage';
-import 'aos/dist/aos.css';
-import Aos from 'aos';
-import { useEffect } from 'react';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
+import LayoutWithoutHeader from "./components/Layout/LayoutWithoutHeader";
+import MainHome from "./Pages/HomePage/MainHome";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+import LoginPage from "./pages/AuthenPage/LoginPage";
+import SignUpPage from "./pages/AuthenPage/SignUpPage";
+import ServicePage from "./Pages/Service/ServicePage";
+import ServiceDetails from "./Pages/Service/ServiceDetails";
+import BlogsPage from "./Pages/BlogsPage/BlogsPage";
+import BlogsDetails from "./Pages/BlogsPage/BlogsDetails";
+import DoctorsPage from "./Pages/Pages/DoctorsPage";
+import DoctorsDetailsPage from "./Pages/Pages/DoctorsDetailsPage";
+import TimeTablePage from "./Pages/Pages/TimeTablePage";
+import PortfolioPage from "./Pages/Pages/PortfolioPage";
+import ErrorPage from "./Pages/Pages/ErrorPage";
+import ContactPage from "./Pages/ContactPage/ContactPage";
+import Appointments from "./Pages/Pages/Appointments";
+import AppointmentsHistory from "./Pages/Pages/AppointmentsHistory";
+import UpdateAppointment from "./Pages/Pages/UpdateAppointment";
+import DoctorAppointments from "./Pages/Pages/DoctorAppointments";
+import AppointmentDetail from "./Pages/Pages/AppointmentDetail";
+import ScrollUpButton from "./Components/ScrollUpButton";
+import DoctorsResultPage from "./Pages/Pages/DoctorsResultPage";
+import "aos/dist/aos.css";
+import Aos from "aos";
+import { useEffect } from "react";
 
 function App() {
   Aos.init({
     duration: 1500,
     delay: 0.25,
-    disable: 'mobile',
+    disable: "mobile",
   });
   const { pathname } = useLocation();
 
@@ -44,11 +42,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout isTopBar={true} />}>
           <Route index element={<MainHome />} />
-          <Route path="/home-v2" element={<HomeV2 />} />
         </Route>
-        <Route path="/" element={<Layout variant="cs_type_1" />}>
-          <Route path="/home-v3" element={<HomeV3 />} />
-        </Route>
+        <Route path="/" element={<Layout variant="cs_type_1" />}></Route>
         <Route path="/" element={<Layout />}>
           <Route path="/about" element={<AboutPage />} />
           <Route path="/service" element={<ServicePage />} />
@@ -56,8 +51,14 @@ function App() {
           <Route path="/blog" element={<BlogsPage />} />
           <Route path="/blog/:blogId" element={<BlogsDetails />} />
           <Route path="/appointments" element={<Appointments />} />
-          <Route path="/appointmentshistory" element={<AppointmentsHistory />} />
-          <Route path="/updateappointment/:appointmentId" element={<UpdateAppointment />} />
+          <Route
+            path="/appointmentshistory"
+            element={<AppointmentsHistory />}
+          />
+          <Route
+            path="/updateappointment/:appointmentId"
+            element={<UpdateAppointment />}
+          />
           <Route path="/doctorappointments" element={<DoctorAppointments />} />
           <Route path="/appointment/:id" element={<AppointmentDetail />} />
           <Route path="/doctors" element={<DoctorsPage />} />
