b793e3194cb5159edd0082ad0ce0822227e92638
Duong <sdoalala0912@gmail.com>
2025-03-03 14:44:19 +0700

    test

--- server/src/routes/scheduleRouter.ts
+++ server/src/routes/scheduleRouter.ts
@@ -1,11 +1,11 @@
 // src/routes/scheduleRoutes.ts
 import express from 'express';
-import { createSchedule, viewSchedule } from '../controllers/schedule/scheduleController';
+import { createSchedule, getAllSchedules, getSchedulesByDoctor,  } from '../controllers/schedule/scheduleController';
 import { authenticateToken, authorizeRole } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
 router.post("/create",authenticateToken, createSchedule, authorizeRole(['doctor'])); // Create schedule
-router.get("/view/:doctorId",authenticateToken, viewSchedule ); // View doctor schedule
-
+router.get("/schedule/:doctorId",authenticateToken, getSchedulesByDoctor ); // View doctor schedule
+router.get("/schedules",authenticateToken, getAllSchedules ); // View all schedules
 export default router;
