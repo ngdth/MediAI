9e4a2de17ec28e666ce69e4aee8b08563ee2ec77
Duong <sdoalala0912@gmail.com>
2025-03-06 22:45:26 +0700

    done loginGoogle

--- server/src/utils/app.ts
+++ server/src/utils/app.ts
@@ -3,11 +3,16 @@ import cors from 'cors'
 import { ORIGIN } from '../constants/index'
 import dotenv from 'dotenv';
 
+import passport from 'passport';
+
 dotenv.config();
 // initialize app
 const app = express()
 
 // middlewares
+
+app.use(passport.initialize());
+
 app.use(cors({ origin: ORIGIN }))
 app.use(express.json()) // body parser
 app.use(express.urlencoded({ extended: false })) // url parser
