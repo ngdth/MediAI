790795605a7b65eb8c30c7d9e69df03f89ba0d31
theanhntp <theanhntp@gmail.com>
2025-04-29 23:24:46 +0700

    change

--- server/src/config/google.ts
+++ server/src/config/google.ts
@@ -5,6 +5,6 @@ dotenv.config();
 export const config = {
     googleClientId: process.env.GOOGLE_CLIENT_ID || '',
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    googleCallbackUrl: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/google/callback',
+    googleCallbackUrl: process.env.GOOGLE_CALLBACK_URL || 'https://amma-care.com/auth/google/callback',
     sessionSecret: process.env.SESSION_SECRET || 'secret-key'
 };
